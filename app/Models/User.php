65d6d0ff24b60a00df728cf39027a476c12154e5
hayashi-hazuki <hayashi_hazuki@tenda.co.jp>
2023-04-08 03:13:54 +0900

    20230408_検証

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
         'password',
         'mail',
     ];
+
+    public function getData()
+    {
+        $data = DB::table($this->table)->get();
+
+        return $data;
+    }
 }
