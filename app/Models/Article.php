5d559797a5772ee5f210d2f6dddd8a887eaf8fd2
hayashi-hazuki <hayashi_hazuki@tenda.co.jp>
2023-04-09 23:17:38 +0900

    20230409

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -81,7 +81,7 @@ class Article extends Authenticatable
             'count(d.reply_id) as reply_cnt'
         ];
         $select_query = DB::table($this->table)
-            ->select($column)
+            ->select(DB::raw($column))
             ->leftJoin('genre_tbl as a', 'article_tbl.genre_id', '=', 'a.genre_id')
             ->leftJoin('chara_tbl as b', 'article_tbl.demand_cha_id', '=', 'b.chara_id')
             ->leftJoin('chara_tbl as c', 'article_tbl.give_cha_id', '=', 'c.chara_id')
