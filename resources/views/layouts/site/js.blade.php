b2580a68edf609c6d3c0c3b835a747f50dfa27d8
hayashi-hazuki <hayashi_hazuki@tenda.co.jp>
2023-04-10 00:40:06 +0900

    20230409

--- resources/views/layouts/site/js.blade.php
+++ resources/views/layouts/site/js.blade.php
@@ -1,7 +1,3 @@
 <!-- REQUIRED JS SCRIPTS -->
-<script src="../../js/wpsite/jquery-3.5.1.min.js"></script>
-<script src="../../js/wpsite/bootstrap.min.js"></script>
-<script src="../../js/wpsite/bootstrap.offcanvas.min.js"></script>
-<script src="../../js/wpsite/ofi.min.js"></script>
-<script src="../../js/wpsite/common.js"></script>
+<script src="../../js/wpsite/jquery-3.6.4.min.js"></script>
 @stack('add_js')
