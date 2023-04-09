9904732c273a1368792d0e043e10d24b76740106
hayashi-hazuki <hayashi_hazuki@tenda.co.jp>
2023-04-10 00:36:53 +0900

    20230409

--- resources/views/layouts/site/js.blade.php
+++ resources/views/layouts/site/js.blade.php
@@ -1,2 +1,7 @@
 <!-- REQUIRED JS SCRIPTS -->
+<script src="../../js/wpsite/jquery-3.5.1.min.js"></script>
+<script src="../../js/wpsite/bootstrap.min.js"></script>
+<script src="../../js/wpsite/bootstrap.offcanvas.min.js"></script>
+<script src="../../js/wpsite/ofi.min.js"></script>
+<script src="../../js/wpsite/common.js"></script>
 @stack('add_js')
